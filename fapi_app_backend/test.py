38ddec09aec1483ff6d9c95d00c37af0242d310f
Luis J Gonzalez <luis.glez2015@yahoo.com>
2025-07-05 06:49:55 -0400

    added requirements.txt and the testing file

---
+++ fapi_app_backend/test.py
@@ -0,0 +1,8 @@
+rarity_types = ['◊', '◊◊', '◊◊◊', '◊◊◊◊', '☆', '☆☆', '☆☆', 'Crown Rare']
+
+patata = '◊◊◊◊'
+
+if patata in rarity_types:
+    print('yes')
+elif patata not in rarity_types:
+    print('nah')
